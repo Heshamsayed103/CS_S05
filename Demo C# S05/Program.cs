4308d2798be7f1cd36f5c420e702613faaf2ed2d
Heshamsayed103 <hesham58382004@gmail.com>
2025-07-15 21:30:49 +0300

    Add project files.

---
+++ Demo C# S05/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo_C__S05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
